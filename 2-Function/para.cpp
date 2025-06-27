c31ed9f68ca74c5a068db939fa906592ea339673
Jan <jan810693@gmail.com>
2025-06-27 08:12:46 +0530

    parameterized function

---
+++ 2-Function/para.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b){
+    return a+b;
+}
+int main(){
+    int a, b;
+    cout<<"Enter 2 Numbers: ";
+    cin>>a>>b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is: "<<sum(a,b);
+}
