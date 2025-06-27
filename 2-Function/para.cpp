5bef66a5eb637500b92e2c12558d4e0c5253b67f
Jan <jan810693@gmail.com>
2025-06-27 08:17:37 +0530

    min of two

--- 2-Function/para.cpp
+++ 2-Function/para.cpp
@@ -4,9 +4,19 @@ using namespace std;
 int sum(int a, int b){
     return a+b;
 }
+
+int minOfTwo(int a, int b){
+    if(a<b){
+        return a;
+    }else{
+        return b;
+    }
+}
 int main(){
     int a, b;
     cout<<"Enter 2 Numbers: ";
     cin>>a>>b;
-    cout<<"Sum of "<<a<<" and "<<b<<" is: "<<sum(a,b);
+    cout<<"Sum of "<<a<<" and "<<b<<" is: "<<sum(a,b)<<endl;
+    cout<<"Minimum is "<<minOfTwo(a,b)<<endl;
+    return 0;
 }
