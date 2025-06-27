9982143fe3d71957be849321e86294603b122c58
Jan <jan810693@gmail.com>
2025-06-27 08:06:58 +0530

    function

---
+++ printHello.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int printHello() {
+    cout << "Hello, World!" << endl;
+    return 3;
+}
+
+int main(){
+    cout<<printHello()<<endl;
+    return 0;
+}
